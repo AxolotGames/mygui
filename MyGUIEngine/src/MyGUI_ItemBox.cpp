2912ddb5c3cb167791c7a3a563689eb6b41c1873
Golen <golen87@hotmail.com>
2017-07-26 12:19:38 +0200

    Allows drag'n'drop for other mousebuttons than Left.
    (cherry picked from commit 7655dc528d389e06c568069c75004c09b5852c1c)

--- MyGUIEngine/src/MyGUI_ItemBox.cpp
+++ MyGUIEngine/src/MyGUI_ItemBox.cpp
@@ -599,7 +599,7 @@ namespace MyGUI
 	{
 		mouseButtonPressed(_id);
 
-		if ( MouseButton::Left == _id)
+		if( mDropButton == MouseButton::None )
 		{
 			size_t old = mIndexSelect;
 
@@ -618,7 +618,7 @@ namespace MyGUI
 			}
 
 			// смещение внутри виджета, куда кликнули мышкой
-			mClickInWidget = InputManager::getInstance().getLastPressedPosition(MouseButton::Left) - _sender->getAbsolutePosition();
+			mClickInWidget = InputManager::getInstance().getLastPressedPosition( _id ) - _sender->getAbsolutePosition();
 
 			// отсылаем событие
 			eventMouseItemActivate(this, mIndexSelect);
@@ -634,8 +634,8 @@ namespace MyGUI
 		bool needEvent = !mStartDrop;
 		mouseButtonReleased(_id);
 
-		if (needEvent)
-			eventNotifyItem(this, IBNotifyItemData(getIndexByWidget(_sender), IBNotifyItemData::MouseReleased, _left, _top, _id));
+		//if (needEvent)
+		eventNotifyItem(this, IBNotifyItemData(getIndexByWidget(_sender), IBNotifyItemData::MouseReleased, _left, _top, _id));
 	}
 
 	void ItemBox::notifyRootMouseChangeFocus(Widget* _sender, bool _focus)
