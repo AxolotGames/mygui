7f5c5373612be5c6e0dd09fd6c8cb36651ce39c3
huxs <dc.jarle@hotmail.com>
2019-09-06 15:23:50 +0200

    Registers BlurrySubSkin in SkinEditor aswell.

--- Tools/SkinEditor/Application.cpp
+++ Tools/SkinEditor/Application.cpp
@@ -27,6 +27,7 @@
 #include "ScopeManager.h"
 #include "FactoryManager.h"
 #include "ComponentFactory.h"
+#include "MyGUI_CommonStateInfo.h"
 
 template <> tools::Application* MyGUI::Singleton<tools::Application>::msInstance = nullptr;
 template <> const char* MyGUI::Singleton<tools::Application>::mClassTypeName = "Application";
@@ -69,6 +70,10 @@ namespace tools
 
 		std::string subWidgetCategory = MyGUI::SubWidgetManager::getInstance().getCategoryName();
 		MyGUI::FactoryManager::getInstance().registerFactory<MyGUI::FilterNone>(subWidgetCategory);
+
+		MyGUI::FactoryManager::getInstance().registerFactory<MyGUI::SubSkin>( subWidgetCategory, "BlurrySubSkin" );
+		std::string stateCategoryName = MyGUI::SubWidgetManager::getInstance().getStateCategoryName();
+		MyGUI::FactoryManager::getInstance().registerFactory<MyGUI::SubSkinStateInfo>( stateCategoryName, "BlurrySubSkin" );
 
 		LoadGuiSettings();
 
